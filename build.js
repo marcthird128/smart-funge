7473f85d6ab7c223c2fed826f6ab7fcb7df48fce
Marc Platt <plattmmarc12@gmail.com>
2025-04-04 13:47:53 -0600

    added icon and fine tuned build.js

--- build.js
+++ build.js
@@ -17,32 +17,42 @@ fs.readdir(assetsPath, (err, files) => {
         console.error('Could not build assets: error reading directory');
         return;
     }
+
+    console.log('Building assets...')
     
     // assets.js file content
-    let buildResult = 'export {';
+    let buildResult = 'export default {';
 
     for (let i=0; i<files.length; i++) {
         let fileName = path.join(assetsPath, files[i]);
+
+        // make sure its a file
         if (!fs.statSync(fileName).isFile()) continue;
         
-        fs.readFile(fileName, (err, buffer) => {
-            if (err) {
-                console.error('Could not read file \'' + fileName + '\'');
-                return;
-            }
+        let buffer;
+        // get the file contents
+        try {
+            buffer = fs.readFileSync(fileName);
+        } catch (e) {
+            console.error('Could not read file \'' + files[i] + '\'');
+            continue;
+        }
 
-            // encode to base64
-            const encoded = buffer.toString('base64');
+        // encode to base64
+        const encoded = buffer.toString('base64');
 
-            // add this file to build result
-            buildResult += '"' + fileName.replaceAll('"', '\\"') + '":"' + encoded + '",';
-        });
+        // add this file to build result
+        buildResult += '"' + files[i].replaceAll('"', '\\"') + '":"' + encoded + '",';
+
+        console.log('Built file \'' + files[i] + '\'');
     }
-
+    
+    // end the object
     buildResult += '}';
-
+        
     // write build results
-    fs.writeFile('./build/assets.js', buildResult, () => {
-        console.log('Built assets (result in ./build.assets.js)');
-    });
+    fs.writeFileSync('./build/assets.js', buildResult);
+
+    console.log('Built assets (result in ./build/assets.js)');
+    console.log('');
 })
