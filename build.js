b5b4a9cc9226bbc15b75f5aba41afc679d07753e
Marc Platt <plattmmarc12@gmail.com>
2025-04-04 12:59:55 -0600

    added build script

---
+++ build.js
@@ -0,0 +1,10 @@
+// Smart Funge build tools
+
+console.log('Smart Funge Build Tools');
+console.log('Building...');
+
+
+
+console.log('');
+console.log('Build complete');
+console.log('Typ `npm run test` to test')
